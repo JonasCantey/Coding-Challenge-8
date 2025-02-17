bb966b09b952da2ca7bb13360b9ff6129486c840
Jonas <jonashooncantey@gmail.com>
2025-02-16 21:19:55 -0500

    Task 3 - Service Fee Calculation

--- cc_8.js
+++ cc_8.js
@@ -17,3 +17,25 @@ function calculateDiscount(price, discountRate) {   //declared function calculat
 
 console.log(`Final Price: $${calculateDiscount(100,0.02)}`)
 console.log(`Final Price: $${calculateDiscount(250,0.15)}`)
+
+//Task 3: Arrow Function
+console.log("Task 3: Arrow Function")
+
+const calculateServiceFee = (amount, serviceType) => {  //Declaring the arrow function
+    let serviceFee;             //declaring a variable to be determined by tier of service
+
+    if (serviceType === "Premium") {    //if service type = Premium, service fee is amount * 0.15
+       serviceFee = amount * 0.15
+    } else if (serviceType === "Standard") {    //if service type = standard, service fee is amount * 0.1
+       serviceFee = amount * 0.1 
+    } else if (serviceType === "Basic") {       //if service type = basic, service fee is amount * 0.05
+       serviceFee = amount * 0.05
+    } else {
+        return `Error: Incorrect service tier reported!` //if service type is anything else an error will be thrown
+    }
+
+    return `Service Fee: $${serviceFee.toFixed(2)}` //returned service fee toFixed(2) to show decimal places
+}
+
+console.log(calculateServiceFee(200, "Premium"));
+console.log(calculateServiceFee(500, "Standard"));
